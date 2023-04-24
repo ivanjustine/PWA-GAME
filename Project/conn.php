67596603e3d28d1820969892bb4a3a4288519de5
ivanjustine <109511132+ivanjustine@users.noreply.github.com>
2023-04-24 16:25:28 +0800

    Delete Project directory

--- Project/conn.php
+++
@@ -1,5 +0,0 @@
-<?php
-//connection
-$conn = mysqli_connect("localhost","root","","onlineshop") or die("Item Not Found");
-?>
-
--- Project/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-$title = "Home";
-$content = "With Jcia's Collections, you have the freedom to pick and become creative by personalizing your  own unique style.  Explore our varieties of hottest trends, classier and cheaper preloved clothes. Enjoy your shopping and grab your new fashion style.";
-
-include 'Template.php';
-?>
